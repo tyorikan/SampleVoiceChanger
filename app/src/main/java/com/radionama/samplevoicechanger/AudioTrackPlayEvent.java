ff6cb19167eac9eddf1f83d6a8a0372c73ecb84e
Takayuki Yorikane <yorikane_takayuki@cyberagent.co.jp>
2014-12-08 19:42:18 +0900

    add AudioTrack sound Visualizer

---
+++ app/src/main/java/com/radionama/samplevoicechanger/AudioTrackPlayEvent.java
@@ -0,0 +1,8 @@
+package com.radionama.samplevoicechanger;
+
+/**
+ * Created by yorikane_takayuki on 2014/12/08.
+ */
+public class AudioTrackPlayEvent {
+
+}
--- app/src/main/java/com/radionama/samplevoicechanger/MainActivity.java
+++ app/src/main/java/com/radionama/samplevoicechanger/MainActivity.java
@@ -1,6 +1,18 @@
 package com.radionama.samplevoicechanger;
 
+import com.radionama.samplevoicechanger.visualizer.VisualizerView;
+import com.radionama.samplevoicechanger.visualizer.renderer.BarGraphRenderer;
+import com.radionama.samplevoicechanger.visualizer.renderer.CircleBarRenderer;
+import com.radionama.samplevoicechanger.visualizer.renderer.CircleRenderer;
+import com.radionama.samplevoicechanger.visualizer.renderer.LineRenderer;
+import com.squareup.otto.Subscribe;
+
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
 import android.media.AudioFormat;
+import android.media.AudioManager;
 import android.media.AudioRecord;
 import android.media.AudioTrack;
 import android.media.MediaRecorder;
@@ -8,26 +20,62 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.RadioGroup;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private static int[] sSampleRates = new int[]{8000, 11025, 16000, 22050, 32000, 44100};
+    private static final int RECORDING_SAMPLE_RATE = sSampleRates[2];
 
     private AudioRecord mAudioRec;
     private AudioTrack mAudioTrack;
 
     private boolean mIsRecording = false;
     private int mBufSize = 0;
-    private int mSamplingRate = 0;
+    private int mSamplingRate = sSampleRates[2];
 
-    private static int[] sSampleRates = new int[]{8000, 11025, 22050, 44100};
+    private RadioGroup mRadioGroup;
+    private Button mToggleButton;
 
-    private static short[] sAudioFormat = new short[]{
-            AudioFormat.ENCODING_PCM_8BIT,
-            AudioFormat.ENCODING_PCM_16BIT};
+    private VisualizerView mVisualizerView;
 
-    private static short[] sChannelConfig = new short[]{
-            AudioFormat.CHANNEL_IN_MONO,
-            AudioFormat.CHANNEL_IN_STEREO};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BusProvider.getInstance().register(this);
+
+        mRadioGroup = (RadioGroup) findViewById(R.id.voice_key);
+        mToggleButton = (Button) findViewById(R.id.toggle_button);
+        mVisualizerView = (VisualizerView) findViewById(R.id.visualizer);
+
+        // AudioRecordの作成
+        findAudioRecord();
+
+        mAudioTrack = new AudioTrack(
+                AudioManager.STREAM_SYSTEM,
+                mSamplingRate,
+                AudioFormat.CHANNEL_OUT_MONO,
+                AudioFormat.ENCODING_PCM_16BIT,
+                mBufSize,
+                AudioTrack.MODE_STREAM
+        );
+
+        mToggleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mIsRecording) {
+                    mToggleButton.setText(R.string.start);
+                } else {
+                    mToggleButton.setText(R.string.stop);
+                }
+
+                recodingAndPlayMain();
+            }
+        });
+    }
 
     private void recodingAndPlayMain() {
         if (mIsRecording) {
@@ -35,54 +83,26 @@ public class MainActivity extends ActionBarActivity {
         } else {
             // 録音開始
             Log.v("AudioRecord", "startRecording");
+
             mAudioRec.startRecording();
             mIsRecording = true;
+
             // 録音スレッド
             new Thread(new Runnable() {
                 @Override
                 public void run() {
+                    setPlaybackRate();
                     recodingAndPlay();
                 }
             }).start();
         }
     }
 
-    public void findAudioRecord() {
-        for (int rate : sSampleRates) {
-            for (short audioFormat : sAudioFormat) {
-                for (short channelConfig : sChannelConfig) {
-                    try {
-                        Log.d("TAG", "Attempting rate " + rate + "Hz, bits: "
-                                + audioFormat + ", channel: " + channelConfig);
-
-                        int bufferSize = AudioRecord
-                                .getMinBufferSize(rate, channelConfig, audioFormat);
-
-                        if (bufferSize != AudioRecord.ERROR_BAD_VALUE) {
-                            // check if we can instantiate and have a success
-                            mAudioRec = new AudioRecord(
-                                    MediaRecorder.AudioSource.MIC,
-                                    rate,
-                                    AudioFormat.CHANNEL_IN_MONO,
-                                    audioFormat,
-                                    bufferSize
-                            );
-                            if (mAudioRec.getState() == AudioRecord.STATE_INITIALIZED) {
-                                mBufSize = bufferSize;
-                            }
-                            mSamplingRate = rate;
-
-                            return;
-                        }
-                    } catch (Exception e) {
-                        Log.e("TAG", rate + "Exception, keep trying.", e);
-                    }
-                }
-            }
-        }
-    }
-
     private void recodingAndPlay() {
+        mAudioTrack.setPlaybackRate(mSamplingRate);
+        mAudioTrack.play();
+        BusProvider.getInstance().post(new AudioTrackPlayEvent());
+
         byte buf[] = new byte[mBufSize];
         while (mIsRecording) {
             // 録音データ読み込み, 再生処理
@@ -93,33 +113,57 @@ public class MainActivity extends ActionBarActivity {
         Log.v("AudioRecord", "stop");
         mAudioRec.stop();
         mAudioTrack.stop();
+        mVisualizerView.setVisualizerEnabled(false);
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public void findAudioRecord() {
+        try {
+            int bufferSize = AudioRecord.getMinBufferSize(
+                    RECORDING_SAMPLE_RATE,
+                    AudioFormat.CHANNEL_IN_MONO,
+                    AudioFormat.ENCODING_PCM_16BIT
+            );
 
-        // AudioRecordの作成
-        findAudioRecord();
-
-        mAudioTrack = new AudioTrack(
-                MediaRecorder.AudioSource.MIC,
-                mSamplingRate,
-                AudioFormat.CHANNEL_OUT_MONO,
-                AudioFormat.ENCODING_PCM_16BIT,
-                mBufSize,
-                AudioTrack.MODE_STREAM
-        );
-        mAudioTrack.setPlaybackRate(sSampleRates[1]);
-        mAudioTrack.play();
-
-        findViewById(R.id.btProc).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                recodingAndPlayMain();
+            mAudioRec = new AudioRecord(
+                    MediaRecorder.AudioSource.MIC,
+                    RECORDING_SAMPLE_RATE,
+                    AudioFormat.CHANNEL_IN_MONO,
+                    AudioFormat.ENCODING_PCM_16BIT,
+                    bufferSize
+            );
+            if (mAudioRec.getState() == AudioRecord.STATE_INITIALIZED) {
+                mBufSize = bufferSize;
             }
-        });
+
+            return;
+
+        } catch (Exception e) {
+            Log.e("TAG", RECORDING_SAMPLE_RATE + "Exception, keep trying.", e);
+        }
+    }
+
+    private void setPlaybackRate() {
+        int id = mRadioGroup.getCheckedRadioButtonId();
+        switch (id) {
+            case R.id.voice_lowest:
+                mSamplingRate = sSampleRates[0];
+                break;
+            case R.id.voice_lower:
+                mSamplingRate = sSampleRates[1];
+                break;
+//            case R.id.voice_default:
+//                mSamplingRate = sSampleRates[2];
+//                break;
+            case R.id.voice_higher:
+                mSamplingRate = sSampleRates[3];
+                break;
+            case R.id.voice_very_higher:
+                mSamplingRate = sSampleRates[4];
+                break;
+            case R.id.voice_highest:
+                mSamplingRate = sSampleRates[5];
+                break;
+        }
     }
 
     @Override
@@ -127,5 +171,64 @@ public class MainActivity extends ActionBarActivity {
         mAudioRec.release();
         mAudioTrack.release();
         super.onDestroy();
+    }
+
+    @Subscribe
+    public void onAudioRecordEvent(AudioTrackPlayEvent event) {
+        mVisualizerView.link(mAudioTrack);
+        addLineRenderer();
+//        addBarGraphRenderers();
+//        addCircleBarRenderer();
+//        addCircleRenderer();
+    }
+
+    // Methods for adding renderers to visualizer
+    private void addBarGraphRenderers() {
+        Paint paint = new Paint();
+        paint.setStrokeWidth(50f);
+        paint.setAntiAlias(true);
+        paint.setColor(Color.argb(200, 56, 138, 252));
+        BarGraphRenderer barGraphRendererBottom = new BarGraphRenderer(16, paint, false);
+        mVisualizerView.addRenderer(barGraphRendererBottom);
+
+        Paint paint2 = new Paint();
+        paint2.setStrokeWidth(12f);
+        paint2.setAntiAlias(true);
+        paint2.setColor(Color.argb(200, 181, 111, 233));
+        BarGraphRenderer barGraphRendererTop = new BarGraphRenderer(4, paint2, true);
+        mVisualizerView.addRenderer(barGraphRendererTop);
+    }
+
+    private void addCircleBarRenderer() {
+        Paint paint = new Paint();
+        paint.setStrokeWidth(8f);
+        paint.setAntiAlias(true);
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.LIGHTEN));
+        paint.setColor(Color.argb(255, 222, 92, 143));
+        CircleBarRenderer circleBarRenderer = new CircleBarRenderer(paint, 32, true);
+        mVisualizerView.addRenderer(circleBarRenderer);
+    }
+
+    private void addCircleRenderer() {
+        Paint paint = new Paint();
+        paint.setStrokeWidth(3f);
+        paint.setAntiAlias(true);
+        paint.setColor(Color.argb(255, 222, 92, 143));
+        CircleRenderer circleRenderer = new CircleRenderer(paint, true);
+        mVisualizerView.addRenderer(circleRenderer);
+    }
+
+    private void addLineRenderer() {
+        Paint linePaint = new Paint();
+        linePaint.setStrokeWidth(1f);
+        linePaint.setAntiAlias(true);
+        linePaint.setColor(Color.argb(88, 0, 128, 255));
+
+        Paint lineFlashPaint = new Paint();
+        lineFlashPaint.setStrokeWidth(5f);
+        lineFlashPaint.setAntiAlias(true);
+        lineFlashPaint.setColor(Color.argb(188, 255, 255, 255));
+        LineRenderer lineRenderer = new LineRenderer(linePaint, lineFlashPaint, true);
+        mVisualizerView.addRenderer(lineRenderer);
     }
 }
